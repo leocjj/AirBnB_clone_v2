e3ec2652c8ff56a0efa77684d3a562ac8b588705
leocjj <leocj@hotmail.com>
2020-04-12 02:21:30 -0500

    Update 3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,69 +1,71 @@
 #!/usr/bin/python3
-# Fabric script that generates a .tgz archive from the contents
-# of the web_static folder and deploy it to servers
-""" Fabric script to deploy """
-import os
+"""
+script (based on the file 2-do_deploy_web_static.py) that creates and distributes an archive to your web servers, using the function deploy:
+"""
+from os.path import isfile
 from fabric.api import *
 from datetime import datetime
 
-env.hosts = ['35.185.87.254', '52.72.127.245']
+env.hosts = ['3.80.113.159', '104.196.66.20']
 env.user = "ubuntu"
 env.key_filename = "~/.ssh/holberton"
 env.warn_only = True
 
 
 def deploy():
-    """ Do all in one step """
-    file_path = do_pack()
-    if os.path.exists(file_path):
-        deploy = do_deploy(file_path)
-        return deploy
-    else:
+    """ Create and distribute an archive to the web servers """
+    archive_path = do_pack()
+    if archive_path is None:
         return False
-
+    return do_deploy(archive_path)
 
 def do_pack():
-    """ compress files """
+    """ Generate a .tgz archive from the contents of the web_static folder """
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
     file_path = "versions/web_static_" + timestamp + ".tgz"
     local("mkdir -p versions")
-    local("tar -cvzf " + file_path + " web_static")
-    if os.path.exists(file_path):
-        return file_path
-    else:
-        return False
 
+    file_compresed = local("tar -cvzf " + file_path + " web_static")
+    if file_compresed.failed:
+        return None
+    return file_path
 
 def do_deploy(archive_path):
-    """ upload to web servers and deploy """
-    if not os.path.exists(archive_path) and not os.path.isfile(archive_path):
+    """ Distribute an archive to the web servers """
+    if not isfile(archive_path):
         return False
     try:
-        # get file name without extension
-        filename = os.path.splitext(os.path.basename(archive_path))[0]
-        # upload to /tmp dir to server
-        put(local_path=archive_path, remote_path="/tmp")
+        # upload file to server
+        put(archive_path, '/tmp/')
+
+        archive = archive_path.replace('.tgz', '')
+        archive = archive.replace('versions/', '')
+
         # create destination directory
-        run("mkdir -p /data/web_static/releases/" + filename + "/")
+        run("mkdir -p /data/web_static/releases/" + archive + "/")
+
         # uncompress tar file to a directory
-        run("sudo tar -xzf /tmp/" + filename + ".tgz" +
-            " -C /data/web_static/releases/" + filename + "/")
+        run("sudo tar -xzf /tmp/" + archive + ".tgz" +
+            " -C /data/web_static/releases/" + archive + "/")
+
         # Delete file uploaded
-        run("rm /tmp/" + filename + ".tgz")
+        run("rm /tmp/" + archive + ".tgz")
 
         # move files to a previous folder
-        run("mv /data/web_static/releases/" + filename +
-            "/web_static/* /data/web_static/releases/" + filename + "/")
+        run("mv /data/web_static/releases/" + archive +
+            "/web_static/* /data/web_static/releases/" + archive + "/")
 
         # delete that folder
-        run("rm -rf /data/web_static/releases/" + filename +
+        run("rm -rf /data/web_static/releases/" + archive +
             "/web_static")
 
         # delete symbolic link /data/web_static/current
         run("rm -rf /data/web_static/current")
+
         # create a new symbolic link
-        run("ln -s /data/web_static/releases/" + filename +
+        run("ln -s /data/web_static/releases/" + archive +
             "/ /data/web_static/current")
+
         print("New version deployed!")
         return True
     except:
