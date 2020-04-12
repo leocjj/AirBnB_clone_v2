5230b8356b68d632fc6de85bba9f8b9801a6ffee
leocjj <leocj@hotmail.com>
2020-04-12 02:08:40 -0500

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,46 +1,53 @@
 #!/usr/bin/python3
-# fabric script that deploy a file it to servers
-""" Fabric script to deploy """
-import os
+"""
+Module that distributes an archive to your web servers using function do_deploy
+"""
+from os.path import isfile
 from fabric.api import *
 from datetime import datetime
 
-env.hosts = ['35.185.87.254', '52.72.127.245']
+
+env.hosts = ['3.80.113.159', '104.196.66.20']
 env.user = "ubuntu"
 env.key_filename = "~/.ssh/holberton"
 env.warn_only = True
 
 
 def do_deploy(archive_path):
-    """ upload to web servers and deploy """
-    if not os.path.exists(archive_path) and not os.path.isfile(archive_path):
+    """ Distribute an archive to the web servers """
+    if not isfile(archive_path):
         return False
     try:
-        # get file name without extension
-        filename = os.path.splitext(os.path.basename(archive_path))[0]
-        # upload to /tmp dir to server
-        put(local_path=archive_path, remote_path="/tmp")
+        # upload file to server
+        put(archive_path, '/tmp/')
+
+        archive = archive_path.replace('.tgz', '').replace('versions/', '')
+
         # create destination directory
         run("mkdir -p /data/web_static/releases/" + filename + "/")
+
         # uncompress tar file to a directory
-        run("sudo tar -xzf /tmp/" + filename + ".tgz" +
-            " -C /data/web_static/releases/" + filename + "/")
+        run("sudo tar -xzf /tmp/" + archive + ".tgz" +
+            " -C /data/web_static/releases/" + archive + "/")
+
         # Delete file uploaded
-        run("rm /tmp/" + filename + ".tgz")
+        run("rm /tmp/" + archive + ".tgz")
 
         # move files to a previous folder
-        run("mv /data/web_static/releases/" + filename +
-            "/web_static/* /data/web_static/releases/" + filename + "/")
+        run("mv /data/web_static/releases/" + archive +
+            "/web_static/* /data/web_static/releases/" + archive + "/")
 
         # delete that folder
-        run("rm -rf /data/web_static/releases/" + filename +
+        run("rm -rf /data/web_static/releases/" + archive +
             "/web_static")
 
         # delete symbolic link /data/web_static/current
         run("rm -rf /data/web_static/current")
+
         # create a new symbolic link
-        run("ln -s /data/web_static/releases/" + filename +
+        run("ln -s /data/web_static/releases/" + archive +
             "/ /data/web_static/current")
+
         print("New version deployed!")
         return True
     except:
