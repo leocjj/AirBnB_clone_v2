aabbedfcb9d7ab340bf22800a9818cb6b1d2b52b
leocjj <leocj@hotmail.com>
2020-04-11 23:46:53 -0500

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
-# sets up web servers for the deployment of web_static
+# script that sets up your web servers for the deployment of web_static
+
 sudo apt-get -y update
 sudo apt-get -y install nginx
 ALIAS="\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n"
-SRC="/etc/nginx/sites-enabled/default"
+SRC="/etc/nginx/sites-available/default"
 sudo mkdir /data/
 sudo mkdir -p /data/web_static/
 sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-echo "Hello World from:..." > /data/web_static/releases/test/index.html
+echo "Holberton School" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i "30i $ALIAS" $SRC
