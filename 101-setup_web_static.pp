31b6d3fb0c7a42e0bd23aa1e8231bdbe61e19f30
leocjj <leocj@hotmail.com>
2020-04-19 08:55:03 -0500

    Update 101-setup_web_static.pp

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,4 +1,4 @@
-# Puppet server setup
+# Puppet setup server
 
 $html = "<html>
   <head>
@@ -8,26 +8,26 @@ $html = "<html>
   </body>
 </html>\n"
 $root_path = '/data/web_static/'
-$path_arr = ["${root_path}releases/test", "${root_path}shared"]
+$path_arr = ['/data/', $root_path, "${root_path}releases/", "${root_path}releases/test", "${root_path}shared"]
 
 exec { 'update':
-  command => '/usr/bin/apt-get update'
+  command => '/usr/bin/apt-get update',
 }
 
 package { 'nginx':
-  ensure  => installed
+  ensure  => installed,
 }
 
 file { $path_arr:
   ensure => 'directory',
   owner  => 'ubuntu',
-  group  => 'ubuntu'
+  group  => 'ubuntu',
 }
 
 file { "${root_path}releases/test/index.html":
   content => $html,
     owner => 'ubuntu',
-    group => 'ubuntu'
+    group => 'ubuntu',
 }
 
 file { "${root_path}current":
@@ -35,17 +35,18 @@ file { "${root_path}current":
   target => "${root_path}releases/test/",
   force  => yes,
   owner  => 'ubuntu',
-  group  => 'ubuntu'
+  group  => 'ubuntu',
 }
 
 exec { 'sed':
-  command => '/usr/bin/env sed -i "/listen 80 default_server;/a \
-\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\
-\n\t}\n" /etc/nginx/sites-available/default',
-  require => Package['nginx']
+  command => '/usr/bin/env sed -i "/listen 80 default_server/a location \
+/hbnb_static/ { alias /data/web_static/current/;}" \
+/etc/nginx/sites-available/default',
+  require => Package['nginx'],
 }
 
 service { 'nginx':
   ensure  => running,
-  require => Package['nginx']
+  require => Package['nginx'],
 }
+
