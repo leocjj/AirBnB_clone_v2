e4ac0c36f677038da8c5a053a37f2b1745d8fff1
leocjj <leocj@hotmail.com>
2020-04-22 21:05:13 -0500

    Update 1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -6,10 +6,6 @@ from flask import Flask
 app = Flask(__name__)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
-
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """ Print a welcome message """
@@ -20,3 +16,6 @@ def hello_hbnb():
 def hbnb():
     """ Print HBNB """
     return 'HBNB'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
