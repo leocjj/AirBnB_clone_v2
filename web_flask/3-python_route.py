2eff10017c701291b9c7e1b9951607f8098ba6cd
leocjj <leocj@hotmail.com>
2020-04-22 21:17:51 -0500

    Create 3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Start a Flask web application in respond to / and /hbnb
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Print a welcome message """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Print HBNB """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """ Print C <text> with spaces"""
+    return 'C {}'.format(text).replace('_', ' ')
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text='is cool'):
+    """ Print Python with optional <text> """
+    return 'Python {}'.format(text).replace('_', ' ')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
