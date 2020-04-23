8b61999c2469cda385bee43cfcf678525f1ef3fe
leocjj <leocj@hotmail.com>
2020-04-22 21:03:07 -0500

    Pep8

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Start a Flask web application in respond to / 
+Start a Flask web application in respond to /
 """
 from flask import Flask
 app = Flask(__name__)
@@ -8,6 +8,7 @@ app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
