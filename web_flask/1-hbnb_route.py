cd72defc816698cbd39e28c8ef42ecaeabad84b3
leocjj <leocj@hotmail.com>
2020-04-22 20:59:53 -0500

    Task 01

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Start a Flask web application in respond to / and /hbnb
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Print a welcome message """
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Print HBNB """
+    return 'HBNB'
