b52883b73cfca2d6d52dc151728d7afa92282787
leocjj <leocj@hotmail.com>
2020-04-22 21:21:40 -0500

    Update 4-number_route.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -35,6 +35,5 @@ def number(n):
     """ Print n is a number only if n is an integer """
     return '{} is a number'.format(n)
 
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
